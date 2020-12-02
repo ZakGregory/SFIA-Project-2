c4f1bc7d0d86ad09dab132d9a9d61e6069b73e65
ZakGregory <zgregoryk@gmail.com>
2020-12-02 15:21:40 +0000

    Updated service1

--- service1/app.py
+++ service1/app.py
@@ -1,4 +1,19 @@
 from application import app
+import requests 
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    randint = requests.get('http://service2:5001/get_integer/').text
+    randfloat = requests.get('http://service3:5002/get_float/').text
+
+    poststring = randint+","+randfloat
+
+    prize = requests.post('http://service4:5003/post_prize/', animal_response).text
+    printstring= randint + randfloat + prize
+    return printstring
 
 if __name__=="__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
