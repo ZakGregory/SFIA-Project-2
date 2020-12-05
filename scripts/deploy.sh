a39de07d73c32c5d71c7075613fac9f2ae31a8a4
ZakGregory <zgregoryk@gmail.com>
2020-12-05 16:44:00 +0000

    Updated deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
-docker stack deploy --compose-file docker-compose.yaml app
+ssh swarm-manager << EOF
+sudo docker pull zakgregory/practical-project-service1
+sudo docker pull zakgregory/practical-project-service1
+sudo docker pull zakgregory/practical-project-service1
+sudo docker pull zakgregory/practical-project-service1
+
+git clone https://github.com/ZakGregory/Practical-Project-Repo.git
+
+sudo docker stack deploy --compose-file docker-compose.yml app
+EOF
