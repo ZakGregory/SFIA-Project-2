3d8458fb67be9a8706448ef18751e54b4f58096e
ZakGregory <zgregoryk@gmail.com>
2020-12-06 22:43:30 +0000

    Added test service4

---
+++ service4/tests/unit_test.py
@@ -0,0 +1,14 @@
+from flask import url_for
+from flask_testing import TestCase
+
+from app import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestService2(TestBase):
+    def test_attack(self):
+        response = self.client.post('/post_attack',data="1,1")
+        output =  response.data.decode('utf-8')
+        self.assertEqual(output, "hit")
