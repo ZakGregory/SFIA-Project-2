a74e2e1676a2fd976e7390341978dfcd2295bebc
ZakGregory <zgregoryk@gmail.com>
2020-12-06 00:36:48 +0000

    Fixed deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,7 @@ sudo docker pull zakgregory/practical-project-service1
 sudo docker pull zakgregory/practical-project-service1
 
 git clone https://github.com/ZakGregory/Practical-Project-Repo.git
-
+cd Practical-Project-Repo
+docker-compose pull
 sudo docker stack deploy --compose-file docker-compose.yml app
 EOF
